f3595a6ff19081e48b89302b85544fe72ab0d0f2
cdey1 <300920267@cencol.ca>
2018-07-31 14:27:54 -0400

    with error

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const http = require('http');
+const fs = require("fs");
+const path = require("path");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  let filePath = "." + req.url;
+
+  if(filePath == "./") {
+    filePath = "./index.html";
+  }
+
+  let extName = path.extname(filePath);
+  let contentType = "text/html";
+  switch(extName){
+    
+  }
+
 });
 
 server.listen(port, hostname, () => {
